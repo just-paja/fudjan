8e04257d043c6f398e9cae8fe76ad8b23fcd79d4
just-paja <just.paja@gmail.com>
2012-07-08 11:27:36 +0200

    Init: compressed code
     * Prepared init to accept compressed code

--- www/etc/init.d/core.php
+++ www/etc/init.d/core.php
@@ -6,21 +6,29 @@ define("DIR_LOCALES",  'lib/locales');
 define("DIR_TEMPLATE", 'lib/template');
 
 ob_implicit_flush(false);
-
 $Microtime = microtime(true);
 
-require_once ROOT."/lib/include/functions.php";
-require_once ROOT."/lib/include/core.php";
-require_once ROOT."/lib/include/aliases.php";
+
+if (php_sapi_name() == 'cli') {
+	$last = end($argv);
+	$_SERVER['REQUEST_URI'] = $last == 'index.php' ? '/':$last;
+}
+
+
+if (!defined('YAWF_PACKED')) {
+	require_once ROOT."/lib/include/functions.php";
+	require_once ROOT."/lib/include/core.php";
+	require_once ROOT."/lib/include/aliases.php";
+}
+
 require_once ROOT."/etc/init.d/session.php";
 
-if (!defined("YACMS_ERROR_HANDLERS")) {
-
-	define("YACMS_ERROR_HANDLERS", true);
+if (!defined("YAWF_ERROR_HANDLERS")) {
+	define("YAWF_ERROR_HANDLERS", true);
 	set_exception_handler(array("System\Status", "catch_exception"));
 	//set_error_handler("handleError");
-
 }
+
 
 System\Settings::init();
 System\Cache::init();
@@ -28,6 +36,7 @@ System\Locales::init();
 System\Database::init();
 System\Input::init();
 System\Output::init();
+
 
 error_reporting(E_ALL);
 //error_reporting(E_ERROR);
