f12a2b46f1dc7a112e6fb5be407f05a1e6f52916
just-paja <just.paja@gmail.com>
2012-07-20 19:55:23 +0200

    Scripts and tools
     * Added js object to handle scripts effectively

---
+++ www/share/scripts/essential/01-functions.js
@@ -0,0 +1,6 @@
+function clog(msg)
+{
+	if (typeof console != 'undefined' && typeof console.log == 'function') {
+		console.log(msg);
+	}
+};
---
+++ www/share/scripts/modules/yawf/tooltip.js
@@ -0,0 +1,73 @@
+yawf_modules.tooltip = function()
+{
+	this.options = {
+		xOffset: 10,
+		yOffset: 25,
+		tooltipId: "yawf_tooltip",
+		clickRemove: false,
+		content: "",
+		useElement: ""
+	};
+
+	$("body").append("<div id='"+ this.options.tooltipId +"'></div>");
+	this.lastTitle = '';
+	this.el = $("body").children('#'+this.options.tooltipId);
+	this.el.hide();
+
+
+	var calc_horizontal_position = function(mouse_pos)
+	{
+		var x = mouse_pos + yawf.tooltip.options.xOffset;
+		return (($(window).width()/2 - x - yawf.tooltip.getWidth()) < 0) ?
+			x - yawf.tooltip.getWidth() - yawf.tooltip.options.xOffset*4:x;
+	}
+
+
+	this.getWidth = function()
+	{
+		return this.el.width();
+	}
+
+
+	this.hide = function(e)
+	{
+		yawf.tooltip.el.hide();
+		$(this).attr("title", yawf.tooltip.lastTitle);
+	};
+
+
+	this.show = function(e, el)
+	{
+		yawf.tooltip.lastTitle = el.title;
+		$(this).attr("title","");
+
+		if (yawf.tooltip.lastTitle != undefined && yawf.tooltip.lastTitle != "") {
+			yawf.tooltip.el.html(yawf.tooltip.lastTitle).css("position", "fixed").css("display", "inline-block");
+			yawf.tooltip.move(e);
+		}
+
+		e.stopPropagation();
+	};
+
+
+	this.move = function(e)
+	{
+		yawf.tooltip.el
+			.css("top", (e.pageY - yawf.tooltip.options.yOffset) + "px")
+			.css("left", calc_horizontal_position(e.pageX) + "px");
+	};
+
+
+	this.init = function()
+	{
+		objects = $(typeof yawf_tooltip_selector == 'undefined' ?
+			'a[title], span[title]':yawf_tooltip_selector
+		);
+
+		objects.each(function() {
+			$(this).hover(function(e) { yawf.tooltip.show(e, this) }, function (e) { yawf.tooltip.hide(e, this) });
+			$(this).mousemove(function(e) { yawf.tooltip.move(e); });
+		});
+		return true;
+	}
+}
