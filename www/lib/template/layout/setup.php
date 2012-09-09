e3bc13464823efa844f835631c54190e44d8ebcc
just-paja <just.paja@gmail.com>
2012-09-09 21:24:11 +0200

    [fix] Setup template

--- www/lib/template/layout/setup.php
+++ www/lib/template/layout/setup.php
@@ -4,30 +4,34 @@ echo '<!DOCTYPE html>';
 echo '<html xmlns="http://www.w3.org/1999/xhtml">';
 
 echo Tag::head();
-	echo Tag::link(array(
-		"rel"   => 'stylesheet',
-		"type"  => 'text/css',
-		"href"  => '/share/styles/default.css',
-		"close" => true,
-	));
 
+	content_for('styles', 'installer');
 	content_from('head');
 
-echo Tag::close('head');
+Tag::close('head');
 
 echo Tag::body();
-	echo Tag::div(array(
-		"id" => 'container'
+	
+	Tag::header(array(
+		"output" => true,
+		"close"  => true,
 	));
+
+	Tag::section(array(
+		"id" => 'container',
+		"output" => true,
+	));
+
 		yield();
 		slot();
 
-	Tag::close('div', true);
-	
-	Tag::footer('footer', array(
+	Tag::close('section', true);
+
+	Tag::footer(array(
 		"content" => System\Output::introduce(),
 		"close" => true,
+		"output" => true,
 	));
 
-	Tag::close('body', true);
-Tag::close('html', true);
+	Tag::close('body');
+Tag::close('html');
