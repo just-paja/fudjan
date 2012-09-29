02217ac3e806c37e068bb0618e0107cde45c41dd
just-paja <just.paja@gmail.com>
2012-09-29 13:37:27 +0200

    Core init: avoiding "@"

--- www/etc/init.d/core.php
+++ www/etc/init.d/core.php
@@ -2,7 +2,12 @@
 
 ob_implicit_flush(false);
 
-@include_once ROOT."/lib/include/system.php";
+/** Find system class package and include it or lookup develop files. You can
+ * unpack it to be accessible by autoloader.
+ */
+if ($sys_package = stream_resolve_include_path(ROOT."/lib/include/system.php")) {
+	require_once $sys_package;
+}
 
 if (!defined('YAWF_PACKED')) {
 	require_once ROOT."/lib/include/functions.php";
