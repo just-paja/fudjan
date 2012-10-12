b4148ba9697fbc8ac289a1ba6f7eabb79e08c6dc
just-paja <just.paja@gmail.com>
2012-10-12 13:48:16 +0200

    Bug page: catch additional errors

--- www/lib/template/errors/bug.html.php
+++ www/lib/template/errors/bug.html.php
@@ -36,20 +36,32 @@ if (!defined("H_BUG_REPORT")) {
 	}
 }
 
+try {
+	$generator = System\Output::introduce();
+} catch(Exception $e) { $generator = 'pwf'; }
+
+$heading = 'There was an error while processing your request';
+$info    = 'We are doing everything in our power to fix that. If you do not share this opinion, please let us know.';
+
+try {
+	$heading = l($heading);
+	$info = l($info);
+} catch(Exception $e) {}
+
 ?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<meta name="generator" content="<?=System\Output::introduce()?>" />
+		<meta name="generator" content="<?=$generator?>" />
 		<meta name="generated-at" content="Sat, 20 Aug 2011 18:52:40 Europe/Prague" />
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<link rel="stylesheet" type="text/css" href="/share/styles/global.css.php?" />
 		<title>Systémová chyba!</title>
 	</head>
-	
+
 	<body class="fatal-error">
 		<section>
-			<h1><?=l('Stala se chyba')?></h1>
-			<p><?=l('Omlouváme se, na webu se vyskytla fatální chyba, kterou nešlo obejít. Pochopitelně v současné době děláme všechno pro to, abychom tuto chybu odstranili.')?></p>
+			<h1><?=$heading?></h1>
+			<p><?=$info?></p>
 			<ul>
 				<?
 					$errors = System\Status::format_errors($desc);
