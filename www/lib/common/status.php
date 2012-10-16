b83ea98d5d9955eafd1da0ab8b20bce3d1407255
just-paja <just.paja@gmail.com>
2012-10-16 19:25:54 +0200

    PWF Status page

---
+++ www/lib/common/status.php
@@ -0,0 +1,48 @@
+<?
+
+define('ROOT', realpath(__DIR__.'/../../'));
+
+require_once ROOT."/etc/init.d/core.php";
+
+System\Init::full();
+
+$db_status = array();
+$cfg_status = array();
+$db = cfg('database', 'list');
+
+foreach ($db as $db_key=>$db_cfg) {
+	$connected = System\Database::is_connected($db_key);
+	$db_status["Database ".$db_key] = array(
+		"message" => $connected ? l('Connected'):l('Not connected'),
+		"class"   => $connected ? 'status-ok':'status-bad',
+	);
+}
+
+$page_tree = cfg('pages');
+unset($page_tree['cron'], $page_tree['api'], $page_tree['ajax-api']);
+$empty = empty($page_tree);
+
+$cfg_status['Page tree'] = array(
+	"message" => $empty ? l('Seems to be empty'):l('Seems to be ok'),
+	"class" => $empty ? 'status-ok':'status-bad',
+);
+
+$htaccess_current = sha1(System\Router::generate_htaccess());
+$htaccess_system = sha1(file_get_contents(ROOT.'/.htaccess'));
+$ok = $htaccess_current == $htaccess_system;
+
+$cfg_status['Apache mod rewrite'] = array(
+	"message" => $ok ? l('Seems to be ok'):l('Does not fit current version'),
+	"class"   => $ok ? 'status-ok':'status-bad',
+);
+
+$status = array(
+	"Database" => $db_status,
+	"Settings" => $cfg_status,
+);
+
+System\Template::insert('system/status-full', array("status" => $status));
+
+System\Output::set_template('status');
+System\Output::set_format('html');
+System\Output::out();
---
+++ www/lib/template/layout/status.php
@@ -0,0 +1,25 @@
+<?
+
+Tag::doctype();
+Tag::html();
+
+	Tag::head();
+		content_for('styles', 'installer');
+		content_from('head');
+	Tag::close('head');
+
+	Tag::body();
+
+		Tag::section(array(
+			"id" => 'container',
+			"output" => true,
+		));
+
+			yield();
+			slot();
+
+		Tag::close('section', true);
+		Tag::footer(array("content" => System\Output::introduce()));
+
+	Tag::close('body');
+Tag::close('html');
