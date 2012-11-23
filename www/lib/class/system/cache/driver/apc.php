7e17433ea71c2edd930eff0eecac44736a8a15e7
just-paja <pavel@proboston.net>
2012-11-23 12:01:11 +0100

    PHP APC cache
    * Added support for PHP APC

--- www/lib/class/system/cache/driver/apc.php
+++ www/lib/class/system/cache/driver/apc.php
@@ -1,10 +1,41 @@
 <?
 
-namespace Cache\Driver
+namespace System\Cache\Driver
 {
-	class Apc implements \Cache\Ifce
+	class Apc implements \System\Cache\Ifce
 	{
+		public function store($path, $value, $ttl)
+		{
+			$res = apc_add($path, $value, $ttl);
+			return $res ? $value:NULL;
+		}
 
 
+		public function fetch($path, &$var)
+		{
+			$res = false;
+			$var = apc_fetch($path, $res);
+			return $res ? $var:($var = NULL);
+		}
+
+
+		public function get($path)
+		{
+			$res = false;
+			$var = apc_fetch($path, $res);
+			return isset($res) ? $var:NULL;
+		}
+
+
+		public function release($path)
+		{
+			return apc_delete($path);
+		}
+
+
+		public function flush()
+		{
+			return apc_clear_cache('user');
+		}
 	}
 }
