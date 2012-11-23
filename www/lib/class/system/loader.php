7b0dbe756bcc28b480ef8aebe02583e7ada1a52d
just-paja <just.paja@gmail.com>
2012-11-23 09:09:25 +0100

    Class loader
    * Load only once

--- www/lib/class/system/loader.php
+++ www/lib/class/system/loader.php
@@ -5,12 +5,16 @@ namespace System
 	class Loader
 	{
 		const DIR_CLASS = '/lib/class';
+		private static $loaded = false;
 
 		public static function load_all()
 		{
-			$files = \System\Directory::find_all_files(ROOT.self::DIR_CLASS);
-			foreach ($files as $file) {
-				require_once $file;
+			if (!self::$loaded) {
+				$files = \System\Directory::find_all_files(ROOT.self::DIR_CLASS);
+				foreach ($files as $file) {
+					require_once $file;
+				}
+				$loaded = true;
 			}
 		}
 	}
