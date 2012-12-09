0cc46bd86c8ef144024052443534e2f3923bc7c9
just-paja <just.paja@gmail.com>
2012-12-09 22:08:41 +0100

    Texts
    * Reduced necessary attributes

--- www/lib/class/system/text.php
+++ www/lib/class/system/text.php
@@ -5,10 +5,9 @@ namespace System
 	class Text extends \System\Model\Database
 	{
 		static $attrs = array(
-			"name"   => array('varchar'),
-			"lang"   => array('varchar'),
-			"text"   => array('text'),
-			"public" => array('bool'),
+			"name"    => array('varchar'),
+			"text"    => array('text'),
+			"visible" => array('bool'),
 		);
 	}
 }
