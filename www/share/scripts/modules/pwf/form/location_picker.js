0f3be059270c05fb101ce99ababf038cd0f1596e
just-paja <just.paja@gmail.com>
2013-01-30 01:13:21 +0100

    Forms: Location picker
    * Added location picker BASE

---
+++ www/share/scripts/modules/pwf/form/location_picker.js
@@ -0,0 +1,65 @@
+pwf.register('location_picker', function()
+{
+	var
+		selectors = [".input-location"],
+		instances = {},
+		class_location_picker = function(el)
+		{
+
+		};
+
+
+	this.init = function()
+	{
+		this.scan();
+	};
+
+
+	this.scan = function(container)
+	{
+		var els;
+
+		if (typeof container === 'undefined') {
+			els = $(selectors.join(', '));
+		} else {
+			els = container.find(selectors.join(', '));
+		}
+
+		for (var i = 0; i < els.length; i++) {
+			this.bind($(els[i]));
+		}
+	};
+
+
+	this.bind = function(el)
+	{
+		var id = get_el_id(el);
+
+		if (typeof instances[id] === 'undefined') {
+			var inst = new class_location_picker(el);
+			instances[el.attr('id')] = inst;
+			//~ inst.update_value();
+		}
+
+		return this.get_instance(el.attr('id'));
+	};
+
+
+	this.get_instance = function(id)
+	{
+		return instances[id];
+	};
+
+
+	var get_el_id = function(el)
+	{
+		return typeof el.attr('id') === 'undefined' ? el.attr('id', create_id()):el.attr('id');
+	};
+
+
+	var create_id = function()
+	{
+		var id = "datetime_picker_" + Math.round(Math.random()*1000000);
+		return typeof instances[id] === 'undefined' ? id:create_id();
+	};
+});
