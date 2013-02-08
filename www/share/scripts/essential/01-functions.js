849f16d0c672e0ab8f7946e3363247c7b3b82d0b
just-paja <pavel@proboston.net>
2013-02-08 17:58:46 +0100

    JS functions: is_attached

--- www/share/scripts/essential/01-functions.js
+++ www/share/scripts/essential/01-functions.js
@@ -4,3 +4,13 @@ function v(msg)
 		console.log(msg);
 	}
 };
+
+
+/** Is jQuery object attached to DOM?
+ * @param jQuery ref
+ * @returns bool
+ */
+function is_attached(ref)
+{
+	return ref.parents(':last').is('html');
+}
