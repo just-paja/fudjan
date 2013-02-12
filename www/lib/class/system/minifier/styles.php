0b1acfe099e31705cfabf55dd4609ef1284d5ff8
just-paja <pavel@proboston.net>
2013-02-12 11:27:28 +0100

    Styles minifier
    * Removed whitespace and comments in regexps

--- www/lib/class/system/minifier/styles.php
+++ www/lib/class/system/minifier/styles.php
@@ -78,43 +78,16 @@ namespace System\Minifier
 					$css = preg_replace('/\\s*;\\s*/', ';', $css);
 
 					// remove ws around urls
-					$css = preg_replace('/
-									url\\(      # url(
-									\\s*
-									([^\\)]+?)  # 1 = the URL (really just a bunch of non right parenthesis)
-									\\s*
-									\\)         # )
-							/x', 'url($1)', $css);
+					$css = preg_replace('/url\\(\\s*([^\\)]+?)\\s*\\)/x', 'url($1)', $css);
 
 					// remove ws between rules and colons
-					$css = preg_replace('/
-									\\s*
-									([{;])              # 1 = beginning of block or rule separator
-									\\s*
-									([\\*_]?[\\w\\-]+)  # 2 = property (and maybe IE filter)
-									\\s*
-									:
-									\\s*
-									(\\b|[#\'"-])        # 3 = first character of a value
-							/x', '$1$2:$3', $css);
+					$css = preg_replace('/\\s*([{;])\\s*([\\*_]?[\\w\\-]+)\\s*:\\s*(\\b|[#\'"-])/x', '$1$2:$3', $css);
 
 					// remove ws in selectors
-					$css = preg_replace_callback('/
-									(?:              # non-capture
-											\\s*
-											[^~>+,\\s]+  # selector part
-											\\s*
-											[,>+~]       # combinators
-									)+
-									\\s*
-									[^~>+,\\s]+      # selector part
-									{                # open declaration block
-							/x'
-							,array($this, '_selectorsCB'), $css);
+					$css = preg_replace_callback('/(?:\\s*[^~>+,\\s]+\\s*[,>+~])+\\s*[^~>+,\\s]+{/x' ,array($this, '_selectorsCB'), $css);
 
 					// minimize hex colors
-					$css = preg_replace('/([^=])#([a-f\\d])\\2([a-f\\d])\\3([a-f\\d])\\4([\\s;\\}])/i'
-							, '$1#$2$3$4$5', $css);
+					$css = preg_replace('/([^=])#([a-f\\d])\\2([a-f\\d])\\3([a-f\\d])\\4([\\s;\\}])/i', '$1#$2$3$4$5', $css);
 
 					// remove spaces between font families
 					$css = preg_replace_callback('/font-family:([^;}]+)([;}])/'
@@ -129,11 +102,7 @@ namespace System\Minifier
 					$css = preg_replace('/([\\w#\\.\\*]+)\\s+([\\w#\\.\\*]+){/', "$1\n$2{", $css);
 
 					// Use newline after 1st numeric value (to limit line lengths).
-					$css = preg_replace('/
-							((?:padding|margin|border|outline):\\d+(?:px|em)?) # 1 = prop : 1st numeric value
-							\\s+
-							/x'
-							,"$1\n", $css);
+					$css = preg_replace('/((?:padding|margin|border|outline):\\d+(?:px|em)?)\\s+/x',"$1\n", $css);
 
 					// prevent triggering IE6 bug: http://www.crankygeek.com/ie6pebug/
 					$css = preg_replace('/:first-l(etter|ine)\\{/', ':first-l$1 {', $css);
@@ -179,13 +148,7 @@ namespace System\Minifier
 
 				if ($this->_inHack) {
 					// inversion: feeding only to one browser
-					if (preg_match('@
-								^/               # comment started like /*/
-								\\s*
-								(\\S[\\s\\S]+?)  # has at least some non-ws content
-								\\s*
-								/\\*             # ends like /*/ or /**/
-						@x', $m, $n)) {
+					if (preg_match('@^/\\s*(\\S[\\s\\S]+?)\\s*/\\*@x', $m, $n)) {
 						// end hack mode after this comment, but preserve the hack and comment content
 						$this->_inHack = false;
 						return "/*/{$n[1]}/**/";
