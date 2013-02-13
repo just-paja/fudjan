93e44cd432abd7858a6c8825e72cf6f2da3360db
just-paja <pavel@proboston.net>
2013-02-13 14:05:59 +0100

    Gps, Location
    * Added Google map functions for objects

--- www/lib/class/system/gps.php
+++ www/lib/class/system/gps.php
@@ -4,9 +4,22 @@ namespace System
 {
 	class Gps
 	{
+		const GMAP_TYPE_ROADMAP   = 'roadmap';
+		const GMAP_TYPE_SATELLITE = 'satellite';
+		const GMAP_TYPE_HYBRID    = 'hybrid';
+		const GMAP_TYPE_TERRAIN   = 'terrain';
+
+		const MAP_WIDTH_DEFAULT = 200;
+		const MAP_HEIGHT_DEFAULT = 200;
+
+
 		private $data = array("lat" => null, "lng" => null);
 
 
+		/** Get latitude
+		 * @param float $val Set latitude to this val
+		 * @returns float
+		 */
 		public function lat($val = null)
 		{
 			!is_null($val) && $this->data['lat'] = floatval($val);
@@ -14,6 +27,10 @@ namespace System
 		}
 
 
+		/** Get longitude
+		 * @param float $val Set longitude to this val
+		 * @returns float
+		 */
 		public function lng($val = null)
 		{
 			!is_null($val) && $this->data['lng'] = floatval($val);
@@ -21,12 +38,54 @@ namespace System
 		}
 
 
+		/* Get formatted latitude
+		 * @returns string
+		 */
+		public function latf()
+		{
+			return $this->format($this->lat());
+		}
+
+
+		/* Get formatted longitude
+		 * @returns string
+		 */
+		public function lngf()
+		{
+			return $this->format($this->lng());
+		}
+
+
+		/* Format float value
+		 * @returns string
+		 */
+		private function format($num)
+		{
+			return number_format($num, 15, '.', '');
+		}
+
+
+		/* Get formatted gps coordinates
+		 * @returns string
+		 */
+		public function gps()
+		{
+			return $this->latf().','.$this->lngf();
+		}
+
+
+		/* Convert object data to JSON
+		 * @returns string
+		 */
 		public function to_json()
 		{
 			return json_encode($this->get_data());
 		}
 
 
+		/* Convert object data to SQL format
+		 * @returns string
+		 */
 		public function to_sql()
 		{
 			$data = array($this->lat(), $this->lng());
@@ -34,24 +93,40 @@ namespace System
 		}
 
 
+		/* Get object data
+		 * @returns array
+		 */
 		public function get_data()
 		{
 			return $this->data;
 		}
 
 
+		/* Create object from separate coordinates
+		 * @param float $lat
+		 * @param float $lng
+		 * @returns self
+		 */
 		public static function from_latlng($lat, $lng)
 		{
 			return  self::from_array(array("lat" => $lat, "lng" => $lng));
 		}
 
 
+		/* Create object from json data
+		 * @param string $str
+		 * @returns self
+		 */
 		public static function from_json($str)
 		{
 			return self::from_array(json_decode($str, true));
 		}
 
 
+		/* Create object from SQL format
+		 * @param string $sql
+		 * @returns self
+		 */
 		public static function from_sql($str)
 		{
 			$str = substr($str, strlen('POINT('));
@@ -61,12 +136,36 @@ namespace System
 		}
 
 
+		/* Create object from data
+		 * @param array $data
+		 * @returns self
+		 */
 		public static function from_array(array $data)
 		{
 			$item = new self();
 			$item->lat($data['lat']);
 			$item->lng($data['lng']);
 			return $item;
+		}
+
+
+		/** Returns URL to the Google Static Maps
+		 * @param int $w Width
+		 * @param int $h Height
+		 * @param string $type
+		 */
+		public function map($w = self::MAP_WIDTH_DEFAULT, $h = self::MAP_HEIGHT_DEFAULT, $type = self::GMAP_TYPE_ROADMAP)
+		{
+			return 'http://maps.googleapis.com/maps/api/staticmap?sensor=false&amp;size='.$w.'x'.$h.'&amp;maptype='.$type.'&amp;markers='.$this->gps();
+		}
+
+
+		/** Get link to the Google Maps
+		 * @returns string
+		 */
+		public function map_link()
+		{
+			return 'http://maps.google.com/maps?q='.$this->gps();
 		}
 	}
 }
--- www/lib/class/system/location.php
+++ www/lib/class/system/location.php
@@ -16,5 +16,47 @@ namespace System
 		protected static $belongs_to = array(
 			"user" => array("model" => '\System\User'),
 		);
+
+
+		/** Returns URL to the Google Static Maps
+		 * @param int $w Width
+		 * @param int $h Height
+		 * @param string $type
+		 * @returns string
+		 */
+		public function map($w = \System\Gps::MAP_WIDTH_DEFAULT, $h = \System\Gps::MAP_HEIGHT_DEFAULT, $type = \System\Gps::GMAP_TYPE_ROADMAP)
+		{
+			return 'http://maps.googleapis.com/maps/api/staticmap?sensor=false&amp;zoom=14&amp;size='.$w.'x'.$h.'&amp;maptype='.$type.'&amp;markers='.$this->gps->gps();
+		}
+
+
+		/** Get link to the Google Maps
+		 * @returns string
+		 */
+		public function map_link()
+		{
+			return 'http://maps.google.com/maps?q='.$this->gps->gps();
+		}
+
+
+		/** Get HTML representation of location
+		 * @param int $w Width
+		 * @param int $h Height
+		 * @param string $type
+		 * @returns string
+		 */
+		public function map_html($w = \System\Gps::MAP_WIDTH_DEFAULT, $h = \System\Gps::MAP_HEIGHT_DEFAULT, $type = \System\Gps::GMAP_TYPE_ROADMAP)
+		{
+			return \Tag::a(array(
+				"output"  => false,
+				"class"   => 'location_map',
+				"href"    => $this->map_link(),
+				"content" => \Tag::img(array(
+					"src"    => $this->map($w, $h, $type),
+					"alt"    => sprintf(l('core_location_on_map'), $this->name),
+					"output" => false,
+				)),
+			));
+		}
 	}
 }
