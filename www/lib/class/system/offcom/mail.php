3e1f52aa917a16a4e875beaeb58bd12dea82f2f5
just-paja <pavel@proboston.net>
2013-02-25 17:35:55 +0100

    Offcom email class
    * Rewritten e-mail class
    * Dropped old e-mailer model class

--- www/lib/class/system/offcom/mail.php
+++ www/lib/class/system/offcom/mail.php
@@ -4,68 +4,143 @@ namespace System\Offcom
 {
 	class Mail extends \System\Model\Attr
 	{
-		public static function create(array $rcpt, array $headers, $msg)
-		{
-			$new_headers = array(
-				"X-Mailer"     => \Core\System\Output::introduce(),
-				"Content-Type" => 'text/plain; charset=utf-8',
-			);
+		const STATUS_SENT    = 1;
+		const STATUS_READY   = 2;
+		const STATUS_SENDING = 3;
+		const STATUS_FAILED  = 4;
 
-			foreach ($headers as $header=>$value) {
-				$new_headers[strtolower($header)] = trim($value);
+
+		protected static $attrs = array(
+			"subject"  => array('varchar', "required" => true),
+			"message"  => array('text', "required" => true),
+			"rcpt"     => array('array', "required" => true),
+			"headers"  => array('array'),
+			"from"     => array('string', "is_null" => false),
+			"reply_to" => array('string', "is_null" => false),
+			"status"   => array('int', "is_unsigned" => true),
+		);
+
+
+		protected static $default_headers = array(
+			"Content-Type" => 'text/plain; charset=utf-8',
+		);
+
+
+		/** Create email object
+		 * @param string $subject Subject encoded in UTF-8
+		 * @param string $message Text message encoded in UTF-8
+		 * @param array  $rcpt    Recipients in nice format
+		 * @param string $from    E-mail address of sender in nice format
+		 * @returns new self
+		 */
+		public static function create($subject, $message, array $rcpt, $from = null)
+		{
+			foreach ($rcpt as &$r) {
+				$r = trim($r);
 			}
 
-			$instance = new self($rcpt, $new_headers, $msg);
-			return $instance;
+			return new self(array(
+				"subject" => $subject,
+				"message" => $message,
+				"rcpt"    => $rcpt,
+				"from"    => $from,
+				"status"  => self::STATUS_READY,
+			));
 		}
 
 
-		public function __construct(array $rcpt, array $headers, $msg)
+		/** Create and immediately send a message
+		 * @param string $subject Subject encoded in UTF-8
+		 * @param string $message Text message encoded in UTF-8
+		 * @param array  $rcpt    Recipients in nice format
+		 * @param string $from    E-mail address of sender in nice format
+		 * @returns bool
+		 */
+		public static function post($subject, $message, array $rcpt, $from = null)
 		{
-			$this->rcpt    = $rcpt;
-			$this->headers = $headers;
-			$this->msg     = $msg;
-			$this->status  = self::STATUS_NEW;
+			$msg = self::create($subject, $message, $rcpt, $from);
+			return $msg->send();
 		}
 
 
+		/** Get set of default e-mail headers
+		 * @returns array
+		 */
+		public static function get_default_headers()
+		{
+			if (!isset(self::$default_headers['X-Mailer'])) {
+				self::$default_headers["X-Mailer"] = \System\Output::introduce();
+			}
+
+			return self::$default_headers;
+		}
+
+
+		/** Get sender or default sender if not set
+		 * @returns string
+		 */
+		public function get_sender()
+		{
+			return is_null($this->from) ? cfg('offcom', 'default', 'email_sender'):$this->from;
+		}
+
+
+		/** Validate e-mail before sending
+		 * @returns bool
+		 */
 		private function validate()
 		{
 			foreach ($this->rcpt as $member) {
 				if (!self::isAddrValid($member)) {
-					throw new \System\Error\Format("E-mail address '".$member."' is not valid!");
+					throw new \System\Error\Format(sprintf('Recipient ".$member." is not formatted according to RFC 2822.', $member));
 				}
 			}
+
+			if (self::isAddrValid($this->get_sender())) {
+				throw new \System\Error\Format(sprintf('Sender "%s" is not formatted according to RFC 2822.', $this->get_sender()));
+			}
+
 			return true;
 		}
 
 
+		private function get_encoded_subject()
+		{
+			return '=?UTF-8?B?'.base64_encode($this->subject).'?=';
+		}
+
+
+		/** Send email message object
+		 * @returns int
+		 */
 		public function send()
 		{
-			try {
-				$this->validate();
-			} catch (\System\Error\Format $e) {
-				$this->status = self::STATUS_FAILED;
-				message('error', l("Cannot send e-mail"), $e->msg());
-				return false;
-			}
+			$this->validate();
 
-			$objhead = $this->headers;
-			$body    = array();
-			$headers = array();
+			$body = array();
+			$headers_str = array();
+
 			$rcpt    = implode(', ', $this->rcpt);
+			$headers = $this->get_default_headers();
+			$headers['From'] = $this->get_sender();
+			$headers['Subject'] = $this->get_encoded_subject();
 
-			$objhead['subject'] = '=?UTF-8?B?'.base64_encode($objhead['subject']).'?=';
-
-			foreach ($objhead as $header=>$value) {
-				$headers[] = ucfirst($header).": ".$value;
+			if ($this->reply_to) {
+				if (self::isAddrValid($this->reply_to)) {
+					$headers['Reply-To'] = $this->reply_to;
+				} else throw new \System\Error\Format(sprintf('Reply-To "%s" is not formatted according to RFC 2822.', $this->get_sender()));
 			}
 
-			$body[] = implode("\n", $headers)."\n";
-			$body[] = strip_tags($this->msg);
+			foreach ($headers as $header=>$value) {
+				$headers_str[] = ucfirsts($header).": ".$value;
+			}
+
+			$body[] = implode("\n", $headers_str)."\n";
+			$body[] = strip_tags($this->message);
+			$body = implode("\n", $body);
 			$this->status = self::STATUS_SENDING;
 
-			if (mail($rcpt, $objhead['subject'], '', implode("\n", $body))) {
+			if (mail($rcpt, $this->get_encoded_subject(), '', $body)) {
 				$this->status = self::STATUS_SENT;
 			} else $this->status = self::STATUS_FAILED;
 
@@ -73,10 +148,15 @@ namespace System\Offcom
 		}
 
 
-		private static function isAddrValid($data, $strict = false)
+		/** Validate email address against RFC 2822
+		 * @param string $email
+		 * @param bool   $strict
+		 * @returns bool
+		 */
+		private static function isAddrValid($email, $strict = false)
 		{
 			$regex = $strict ? '/^([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})$/i' : '/^([*+!.&#$|\'\\%\/0-9a-z^_`{}=?~:-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})$/i';
-			return preg_match($regex, trim($data), $matches);
+			return preg_match($regex, trim($email), $matches);
 		}
 
 	}
