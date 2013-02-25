5dcfb5372810ba3999e4b798fb8711443f6d43a1
just-paja <pavel@proboston.net>
2013-02-25 16:42:26 +0100

    Module class
    * Commented functions in module class
    * Added locals require function to modules

--- www/lib/class/system/module.php
+++ www/lib/class/system/module.php
@@ -9,11 +9,6 @@ namespace System
 		static private $array_forced_locals = array("conds", "opts");
 		private $id, $path, $locals, $slot, $parents;
 
-		static public function get_new_id()
-		{
-			return 'noname-'.self::$instances;
-		}
-
 
 		public function __construct($module, $locals = array(), $parents = array())
 		{
@@ -31,31 +26,43 @@ namespace System
 		}
 
 
+		/** Get local path to module
+		 * @returns string
+		 */
 		public function get_path()
 		{
 			return $this->path;
 		}
 
 
+		/** Get module name
+		 * @returns string
+		 */
 		public function get_name()
 		{
-			return _('Modul').' '.$this->path;
+			return t('module_name', $this->path);
 		}
 
 
+		/** Get module id unique on a page
+		 * @returns string
+		 */
 		public function get_id()
 		{
 			return $this->id;
 		}
 
 
+		/** Run module
+		 * @returns void
+		 */
 		public function make()
 		{
 			$path = ROOT.self::BASE_DIR.$this->path.'.php';
 
 			if (file_exists($path)) {
 				if (is_readable($path)) {
-					if (user()->is_root() || user()->has_right_to('*') || user()->has_right_to(substr($this->path, 1))) {
+					if (user()->is_root() || user()->has_right_to('*') || user()->has_right_to(substr($this->get_path(), 1))) {
 						if (!is_array($this->locals)) $this->locals = array($this->locals);
 						$locals = &$this->locals;
 
@@ -73,7 +80,7 @@ namespace System
 
 							foreach (self::$array_forced_locals as $var) {
 								if (isset($locals[$var]) && !is_array($locals[$var])) {
-									throw new \System\Error\Argument(sprintf('Local variable "$%s" must be an array for module "%s"', $var, $this->path));
+									throw new \System\Error\Argument(sprintf('Local variable "$%s" must be an array for module "%s"', $var, $this->get_path()));
 								}
 							}
 
@@ -100,13 +107,30 @@ namespace System
 						}
 
 						return !!$req;
-					} else throw new \System\Error\Permissions(sprintf('Cannot access module "%s". Permission denied.', $this->path));
-				} else throw new \System\Error\Permissions(sprintf('Cannot access module "%s". File is not readable.', $this->path));
-			} else throw new \System\Error\File(sprintf('Module not found: "%s", expected on path "%s".', $this->path, $path));
+					} else throw new \System\Error\Permissions(sprintf('Cannot access module "%s". Permission denied.', $this->get_path()));
+				} else throw new \System\Error\Permissions(sprintf('Cannot access module "%s". File is not readable.', $this->get_path()));
+			} else throw new \System\Error\File(sprintf('Module not found: "%s", expected on path "%s".', $this->get_path(), $path));
 		}
 
 
-		public function template($name, $locals = array())
+		/** Require variable input. Throws exception if variable was not defined in local variable set.
+		 * @param string $var_name
+		 * @returns
+		 */
+		public function req($var_name)
+		{
+			if (!is_null($this->locals[$var_name])) {
+				return $this->locals[$var_name];
+			} else throw new \System\Error\Argument(sprintf('Local variable "%s" must be defined and not null for module "%s"!', $var_name, $this->get_path()));
+		}
+
+
+		/** Insert template into output queue
+		 * @param string $name  Template name begining at /lib/template/partial
+		 * @param array $locals Local variables for the template
+		 * @returns void
+		 */
+		public function template($name, array $locals = array())
 		{
 			if ($name instanceof \System\Form)
 			{
@@ -124,25 +148,17 @@ namespace System
 		}
 
 
-		public function error($e, $desc = null)
+		/** Create new module id
+		 */
+		static public function get_new_id()
 		{
-			switch($e){
-				case 'params':
-					message('error', _('Moduly'), sprintf(_('%s dostal špatné parametry'), $this->get_name()));
-					break;
-				case 'perms':
-					message('error', _('Oprávnění'), sprintf(_('Nemáte oprávnění k provedení akce: %s'), $desc));
-					break;
-				case 'ref-not-found':
-					message('error', _('Moduly'), sprintf(_('%s nenalezl hledaný objekt'), $this->get_name()));
-					break;
-				default:
-					message('error', sprintf(_('V modulu %s se vyskytla chyba'), $this->get_name()), $desc);
-					break;
-			}
+			return 'noname-'.self::$instances;
 		}
 
 
+		/** Get all modules
+		 * @param bool $with_perms
+		 */
 		public static function get_all($with_perms = false)
 		{
 			$mods = array();
@@ -166,12 +182,19 @@ namespace System
 		}
 
 
+		/** Check if module exists
+		 * @param string $mod
+		 */
 		public static function exists($mod)
 		{
 			return file_exists(ROOT.self::BASE_DIR.'/'.$mod.'.php');
 		}
 
 
+		/** Evaluate conditions if the module can be used
+		 * @param array $conds
+		 * @returns bool
+		 */
 		public static function eval_conds(array $conds)
 		{
 			$result = true;
