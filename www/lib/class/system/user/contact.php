aa6d2fabf1f36e2b2f62e476feada53f676ddbf3
just-paja <pavel@proboston.net>
2013-03-07 17:37:53 +0100

    User contact
    * Added some social media to user contact

--- www/lib/class/system/user/contact.php
+++ www/lib/class/system/user/contact.php
@@ -16,18 +16,25 @@ namespace System\User
 		const IM_MSN   = 9;
 		const IM_YAHOO = 10;
 
+		const SOCIAL_FACEBOOK    = 11;
+		const SOCIAL_TWITTER     = 12;
+		const SOCIAL_GOOGLE_PLUS = 13;
+
 		protected static $attrs = array(
 			"type"    => array('int', "is_unsigned" => true, "options" => array(
-				self::STD_EMAIL      => 'contact_type_email',
-				self::STD_MOBILE     => 'contact_type_mobile',
-				self::STD_WEBSITE    => 'contact_type_website',
-				self::STD_PHONE      => 'contact_type_phone',
-				self::IM_XMPP_JABBER => 'contact_type_xmpp_jabber',
-				self::IM_XMPP_GTALK  => 'contact_type_xmpp_gtalk',
-				self::IM_AIM         => 'contact_type_aim',
-				self::IM_ICQ         => 'contact_type_icq',
-				self::IM_MSN         => 'contact_type_msn',
-				self::IM_YAHOO       => 'contact_type_yahoo',
+				self::STD_EMAIL          => 'contact_type_email',
+				self::STD_MOBILE         => 'contact_type_mobile',
+				self::STD_WEBSITE        => 'contact_type_website',
+				self::STD_PHONE          => 'contact_type_phone',
+				self::IM_XMPP_JABBER     => 'contact_type_xmpp_jabber',
+				self::IM_XMPP_GTALK      => 'contact_type_xmpp_gtalk',
+				self::IM_AIM             => 'contact_type_aim',
+				self::IM_ICQ             => 'contact_type_icq',
+				self::IM_MSN             => 'contact_type_msn',
+				self::IM_YAHOO           => 'contact_type_yahoo',
+				self::SOCIAL_FACEBOOK    => 'contact_type_facebook',
+				self::SOCIAL_TWITTER     => 'contact_type_twitter',
+				self::SOCIAL_GOOGLE_PLUS => 'contact_type_google_plus',
 			)),
 			"name"    => array('varchar'),
 			"ident"   => array('varchar'),
