48afb160d7f2c9be5344c9abe281c2179a59b85b
just-paja <pavel@proboston.net>
2013-03-11 15:49:39 +0100

    System status partial
    * Added output format to system status partial
    * Added txt format of system status partial

--- www/lib/template/partial/system/status.html.php
+++ www/lib/template/partial/system/status.html.php
@@ -1,6 +1,7 @@
 <div class="status-dump">
 	<h3>Stystem status</h3>
 	<div class="pre">
+		<strong>Output format:</strong> <?=System\Output::get_format(true)?><br />
 		<strong>Page path:</strong> <?=System\Page::get_path()?><br />
 		<strong>SQL Queries:</strong> <?=System\Query::count_all()?><br />
 		<strong>Exec time:</strong> <?=round(System\Flow::get_exec_time(), 6)?>s
--- www/lib/template/partial/system/status.txt.php
+++ www/lib/template/partial/system/status.txt.php
@@ -1,7 +1,8 @@
 <?
 
 out_flist(array(
-	'Page path'   => System\Page::get_path(),
-	'SQL Queries' => System\Query::count_all(),
-	'Exec time'   => round(System\Flow::get_exec_time(), 6).'s',
+	'Output format' => System\Output::get_format(true),
+	'Page path'     => System\Page::get_path(),
+	'SQL Queries'   => System\Query::count_all(),
+	'Exec time'     => round(System\Flow::get_exec_time(), 6).'s',
 ));
