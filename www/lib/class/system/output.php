37c5f685f89af636f8fc720b638a9ae78932e5c6
just-paja <just.paja@gmail.com>
2013-05-13 21:01:19 +0200

    Output
    * Moved output cfg inside output class

--- www/lib/class/system/output.php
+++ www/lib/class/system/output.php
@@ -4,6 +4,20 @@ namespace System
 {
 	class Output
 	{
+		const FORMAT_HTML  = 'html';
+		const FORMAT_XHTML = 'xhtml';
+		const FORMAT_XML   = 'xml';
+		const FORMAT_JSON  = 'json';
+		const FORMAT_TXT   = 'txt';
+
+		private static $mime = array(
+			self::FORMAT_HTML  => "text/html",
+			self::FORMAT_XHTML => "application/xhtml+xml",
+			self::FORMAT_XML   => "application/xml",
+			self::FORMAT_JSON  => "application/json",
+			self::FORMAT_TXT   => "text/plain",
+		);
+
 		const DIR_TEMPLATE = "/lib/template";
 		const DEFAULT_TEMPLATE = "pwf/default";
 		const DEFAULT_OUT = "html";
@@ -20,11 +34,9 @@ namespace System
 
 		public static function get_mime($format)
 		{
-			try {
-				return cfg('output', 'format', $format);
-			} catch (\System\Error $e) {
-				return 'unknown mime type';
-			}
+			if (isset(self::$mime[$format])) {
+				return self::$mime[$format];
+			} else throw new \System\Error\Argument(sprintf("Unknown mime type format '%s'.", $format));
 		}
 
 
