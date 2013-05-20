6c027170fe16472c6947dd8e3ff5b6301b40014a
just-paja <just.paja@gmail.com>
2013-05-20 12:54:01 +0200

    Forms, template
    * Dropped old form template code

--- www/lib/template/partial/system/form.php
+++ www/lib/template/partial/system/form.php
@@ -15,40 +15,10 @@ foreach ((array) $f->class as $c) {
 }
 
 echo \System\Form\Renderer::render($ren, $f);
-exit;
-Tag::div(array("id" => $f->id.'-container', "class" => array_merge(array('pwform'), $cclass)));
 
-	Tag::a(array("name" => $f->anchor, "id" => $f->anchor, "close" => true));
-	$f->heading && print($renderer->heading($f->heading));
-	$f->desc && Tag::p(array("class" => 'desc', "content" => $f->desc));
-
-	Tag::form($f->get_attr_data());
-
-		Tag::fieldset(array(
-			"class" => 'hidden',
-			"content" => Tag::input(array(
-				"value"  => htmlspecialchars(json_encode($f->get_hidden_data())),
-				"type"   => 'hidden',
-				"name"   => $f->get_prefix().'data_hidden',
-				"close"  => true,
-				"output" => false,
-			)),
-		));
-
-		$objects = $f->get_objects();
-
-		foreach ($objects as $obj) {
-			System\Form\Helper::render_element($obj);
-		}
-
-		echo div('cleaner', '');
-	Tag::close('form');
-Tag::close('div');
-
-
-if (!empty($f->form_js)) {
-	Tag::script(array(
-		"type"    => 'text/javascript',
-		"content" => '$(function() { '.implode("\n", $f->form_js).' });',
-	));
-}
+//~ if (!empty($f->form_js)) {
+	//~ Tag::script(array(
+		//~ "type"    => 'text/javascript',
+		//~ "content" => '$(function() { '.implode("\n", $f->form_js).' });',
+	//~ ));
+//~ }
