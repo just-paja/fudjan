8683cb228094b4b42bfae2b04451f11ec71ec13f
just-paja <pavel@proboston.net>
2013-07-01 17:10:44 +0200

    [js] PWF, inline docs
    * Added inline docs to pwf base script

--- www/share/scripts/pwf.js
+++ www/share/scripts/pwf.js
@@ -2,14 +2,34 @@ var pwf = function()
 {
 	return new function()
 	{
+		/** Status of all registered modules */
 		this.module_status = {};
+
 		var
+			/** Reference to this object accessible from inside this function */
 			self = this,
+
+			/** Queue for modules that are registered but could not be initialized yet */
 			init_later = [],
+
+			/** List of modules that have std scan method */
 			init_scan  = [],
+
+			/** Queue for callbacks that will be run on run_callbacks */
 			callbacks  = [];
 
 
+		/** Register function under name as a module under pwf
+		 *
+		 * Example:
+		 * pwf.register('some_module', function() {
+		 *   this.say_hello = function() { alert('hello'); };
+		 * });
+		 *
+		 * @param string   name
+		 * @param function module
+		 * @return void
+		 */
 		this.register = function(name, module)
 		{
 			if (typeof module == 'function' && typeof this[name] == 'undefined') {
@@ -43,6 +63,9 @@ var pwf = function()
 		};
 
 
+		/** Check if dependencies of all modules that were not initialized yet are met and if so, initialize them
+		 * @return void
+		 */
 		this.init_remaining = function()
 		{
 			for (var i = 0; i<init_later.length; i++) {
@@ -56,7 +79,8 @@ var pwf = function()
 		};
 
 
-		/** Perform a scan of element for all modules with scan method
+		/** Perform a scan of element for all modules with std scan method. Standard scan method takes one not mandatory argument as jQuery object to search and binds its functions to elements found by it's selector.
+		 * @see /share/scripts/pwf/form/date_picker.js
 		 * @param jQuery el
 		 */
 		this.scan = function(el)
@@ -71,12 +95,21 @@ var pwf = function()
 		};
 
 
+		/** Plain getter for scan_list
+		 * @return Object
+		 */
 		this.get_scan_list = function()
 		{
 			return init_scan;
 		};
 
 
+		/** Run lambda callback when listed components are ready
+		 * @param Object   components List (array) of component names (string)
+		 * @param function lambda     Callback to call when ready
+		 * @param Object   args       Arguments to pass to lambda
+		 * @return void
+		 */
 		this.when_ready = function(components, lambda, args)
 		{
 			if (this.components_ready(components)) {
@@ -85,6 +118,9 @@ var pwf = function()
 		};
 
 
+		/** Check if dependencies for callbacks from when_ready() are met and run them if so.
+		 * @return void
+		 */
 		this.run_callbacks = function()
 		{
 			for (var i = 0; i < callbacks.length; i++) {
@@ -98,6 +134,10 @@ var pwf = function()
 		};
 
 
+		/** Dear sir, are all components of this list ready?
+		 * @param Object components List (array) of component names
+		 * @return bool
+		 */
 		this.components_ready = function(components)
 		{
 			var ready = false;
@@ -110,6 +150,10 @@ var pwf = function()
 		};
 
 
+		/** Dear sir, is this component ready to use?
+		 * @param string component Component name
+		 * @return bool
+		 */
 		this.component_ready = function(component)
 		{
 			return typeof this[component] != 'undefined' && this[component].is_ready();
