6254f04a5fedcc5b5cbdb93236ede7a9277ec08f
just-paja <pavel@proboston.net>
2014-01-21 15:11:28 +0100

    Datetime input support

---
+++ www/lib/class/system/form/input/datetime.php
@@ -0,0 +1,30 @@
+<?
+
+namespace System\Form\Input
+{
+	class Datetime extends \System\Form\Input
+	{
+		public function is_valid()
+		{
+			$valid = parent::is_valid();
+			$value = $this->val();
+
+			if ($value) {
+			}
+
+			return $valid;
+		}
+
+
+		public function val_get()
+		{
+			$val = $this->form()->input_value($this->name);
+
+			if ($val && gettype($val) == 'string') {
+				$val = new \DateTime($val);
+			}
+
+			return $val;
+		}
+	}
+}
