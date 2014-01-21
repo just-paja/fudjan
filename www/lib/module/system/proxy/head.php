5334c785ec95d6fedd0acdf1869a34aa2c818e80
just-paja <pavel@proboston.net>
2014-01-21 17:45:15 +0100

    Head proxy, new offcom API
    * Migrated head proxy to new offcom API

--- www/lib/module/system/proxy/head.php
+++ www/lib/module/system/proxy/head.php
@@ -11,27 +11,9 @@ if ($url) {
 	$response = \System\Offcom\Request::get($url, true);
 
 	$data['status'] = $response->status;
-	$data['headers'] = array();
-	$headers = explode("\n", $response->headers);
-
-	foreach ($headers as $row) {
-		$row = trim($row);
-
-		if (strpos($row, ':') > 0) {
-			$row = explode(":", $row, 2);
-			$name = trim($row[0]);
-			$content = trim($row[1]);
-			$data['headers'][$name] = $content;
-		}
-	}
-}
-
-if (isset($data['headers']['Content-Length'])) {
-	$data['size'] = $data['headers']['Content-Length'];
-}
-
-if (isset($data['headers']['Content-Type'])) {
-	$data['type'] = strtolower($data['headers']['Content-Type']);
+	$data['headers'] = $response->headers;
+	$data['size'] = $response->size;
+	$data['type'] = $response->mime;
 }
 
 $ren->partial('system/common', array('json_data' => $data));
