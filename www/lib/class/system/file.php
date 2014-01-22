5100c05b0fbece0fd9405046cc1d873c7d67c506
just-paja <pavel@proboston.net>
2014-01-22 11:23:24 +0100

    Files, reading meta

--- www/lib/class/system/file.php
+++ www/lib/class/system/file.php
@@ -54,7 +54,13 @@ namespace System
 				$path = ROOT.$path;
 			}
 
-			return new self(array("path" => dirname($path), "name" => basename($path), "keep" => true));
+			$file = new self(array(
+				"path" => dirname($path),
+				"name" => basename($path),
+				"keep" => true
+			));
+
+			return $file->read_meta();
 		}
 
 
@@ -605,8 +611,19 @@ namespace System
 				"name"   => $this->name,
 				"mime"   => $this->mime,
 				"size"   => $this->size,
-				"method" => $this->method,
+				"method" => $this->method ? $this->method:'save',
 			);
+		}
+
+
+		public function read_meta()
+		{
+			if ($this->exists()) {
+				$this->size = filesize($this->get_path());
+				$this->mime = self::get_mime($this->get_path());
+			}
+
+			return $this;
 		}
 
 
@@ -629,6 +646,26 @@ namespace System
 		public static function check($path)
 		{
 			return file_exists($path);
+		}
+
+
+		public static function get_mime($file)
+		{
+			if (function_exists("finfo_file")) {
+				$finfo = finfo_open(FILEINFO_MIME_TYPE); // return mime type ala mimetype extension
+				$mime = finfo_file($finfo, $file);
+				finfo_close($finfo);
+				return $mime;
+			} else if (function_exists("mime_content_type")) {
+				return mime_content_type($file);
+			} else if (!stristr(ini_get("disable_functions"), "shell_exec")) {
+				// http://stackoverflow.com/a/134930/1593459
+				$file = escapeshellarg($file);
+				$mime = shell_exec("file -bi " . $file);
+				return $mime;
+			} else {
+				return false;
+			}
 		}
 	}
 }
