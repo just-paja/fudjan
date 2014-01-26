2c2def0daad3ddee53c782f687bc3baebc84d8af
just-paja <just.paja@gmail.com>
2014-01-26 13:35:13 +0100

    Core init, include required classes

--- etc/init.d/core.php
+++ etc/init.d/core.php
@@ -10,18 +10,11 @@ if (!defined('BASE_DIR')) {
 	define('BASE_DIR', ROOT);
 }
 
-/** Find system class package and include it or lookup develop files. You can
- * unpack it to be accessible by autoloader.
- */
-if ($sys_package = stream_resolve_include_path(ROOT."/lib/include/system.php")) {
-	require_once $sys_package;
-}
-
-if (!defined('YAWF_PACKED')) {
-	require_once ROOT."/lib/class/system/loader.php";
-	require_once ROOT."/lib/include/constants.php";
-	require_once ROOT."/lib/include/functions.php";
-	require_once ROOT."/lib/include/core.php";
-	require_once ROOT."/lib/include/aliases.php";
-}
+require_once ROOT."/lib/class/system/loader.php";
+require_once ROOT."/lib/class/system/directory.php";
+require_once ROOT."/lib/class/system/composer.php";
+require_once ROOT."/lib/include/constants.php";
+require_once ROOT."/lib/include/functions.php";
+require_once ROOT."/lib/include/core.php";
+require_once ROOT."/lib/include/aliases.php";
 
