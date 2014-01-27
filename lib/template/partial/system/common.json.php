f2fbbe97406eb5244babf3a54504bf88be04d8c7
just-paja <pavel@proboston.net>
2014-01-27 10:39:37 +0100

    Default JSON template, allow JSONp

--- lib/template/partial/system/common.json.php
+++ lib/template/partial/system/common.json.php
@@ -1,4 +1,10 @@
 <?
 
-echo json_encode($json_data);
+def($allow_jsonp, true);
+
+if ($allow_jsonp && $request->get('callback')) {
+	echo $request->get('callback').'('.json_encode($json_data).');';
+} else {
+	echo json_encode($json_data);
+}
 
