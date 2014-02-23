2947e7be3ef80e6da128a89e9e511a5b22c7cb02
just-paja <just.paja@gmail.com>
2014-02-24 00:53:42 +0100

    Attachment class
    * Prepared attachment lib for creating media library

---
+++ lib/class/system/attachment.php
@@ -0,0 +1,22 @@
+<?
+
+namespace System
+{
+	class Attachment extends \System\Model\Database
+	{
+		const TYPE_FILE  = 1;
+		const TYPE_IMAGE = 2;
+
+		protected static $attrs = array(
+			'name' => array('varchar'),
+			'desc' => array('text', 'is_null' => true),
+			'type' => array('int', 'is_unsigned' => true, 'options' => array(
+				array('value' => self::TYPE_FILE,  'name' => 'attachment_file'),
+				array('value' => self::TYPE_IMAGE, 'name' => 'attachment_image'),
+			)),
+
+			'file' => array('file', 'is_null' => true),
+			'image' => array('image', 'is_null' => true),
+		);
+	}
+}
