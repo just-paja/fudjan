c5df3f848db0c51494969aa4278a266d18f963fc
just-paja <just.paja@gmail.com>
2014-03-18 18:36:46 +0100

    User groups default ctx processor

---
+++ lib/context/user_groups.php
@@ -0,0 +1,11 @@
+<?
+
+$context = function($rq, $res) {
+	if ($rq->user) {
+		return array(
+			'groups' => $rq->user->groups->fetch()
+		);
+	}
+
+	throw new \System\Error\AccessDenied('User is not logged in.');
+};
