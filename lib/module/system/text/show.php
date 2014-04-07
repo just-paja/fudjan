b618c697dbfd090dc6605339a393f15813b8b520
just-paja <pavel@proboston.net>
2014-04-07 18:05:19 +0200

    Texts, use idents

--- lib/module/system/text/show.php
+++ lib/module/system/text/show.php
@@ -1,10 +1,23 @@
 <?
 
-$this->req('id');
+try {
+	$this->req('ident');
+} catch(\System\Error\Argument $e) {
+	$this->req('id');
+}
+
 def($template, 'system/text/show');
 def($show_heading, true);
 
-if ($id && $text = find('\System\Text', $id)) {
+$conds = array();
+
+if (isset($ident)) {
+	$conds['ident'] = $ident;
+} else {
+	$conds['id'] = $id;
+}
+
+if ($text = get_first('\System\Text', $conds)->fetch()) {
 
 	$this->partial($template, array(
 		"text" => $text,
@@ -13,7 +26,7 @@ if ($id && $text = find('\System\Text', $id)) {
 
 } else {
 	if (cfg('dev', 'debug', 'backend')) {
-		throw new \System\Error\Config('Text was not found', $id);
+		throw new \System\Error\Config('Text was not found', isset($ident) ? $ident:$id);
 	} else {
 		throw new \System\Error\NotFound();
 	}
