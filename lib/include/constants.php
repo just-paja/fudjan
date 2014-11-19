7330e2ca04470aceb0f62e4e445afb13311a1fb7
just-paja <pavel@proboston.net>
2014-11-19 14:30:43 +0100

    Define GD constants if missing

--- lib/include/constants.php
+++ lib/include/constants.php
@@ -6,3 +6,15 @@
 
 define("NL",  "\n");
 define("TAB", "\t");
+
+if (!defined('IMG_GIF')) {
+	define('IMG_GIF', 1);
+}
+
+if (!defined('IMG_JPG')) {
+	define('IMG_JPG', 2);
+}
+
+if (!defined('IMG_PNG')) {
+	define('IMG_PNG', 4);
+}
