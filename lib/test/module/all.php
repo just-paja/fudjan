e2b277eaed40618d805ed67949d5e2fc3552c345
just-paja <just.paja@gmail.com>
2015-03-30 19:34:31 +0200

    Test all module sanity

---
+++ lib/test/module/all.php
@@ -0,0 +1,52 @@
+<?
+
+namespace Test\Module
+{
+	class All extends \PHPUnit_Framework_TestCase
+	{
+		public function test_load()
+		{
+			\System\Loader::load_all_modules();
+		}
+
+
+		/**
+		 * @dataProvider module_batch
+		 */
+		public function test_module_syntax($cname)
+		{
+			$this->assertTrue(method_exists($cname, 'run'));
+		}
+
+
+		public static function module_batch()
+		{
+			\System\Loader::load_all_modules();
+
+			$list = static::get_children_of('System\Module');
+			$data = array();
+
+			foreach ($list as $cname) {
+				$data[] = array($cname);
+			}
+
+			return $data;
+		}
+
+
+		public static function get_children_of($parent)
+		{
+			$list = array();
+			$all  = get_declared_classes();
+
+			foreach ($all as $cname) {
+				if (in_array($parent, class_parents($cname))) {
+					$list[] = $cname;
+				}
+			}
+
+
+			return $list;
+		}
+	}
+}
