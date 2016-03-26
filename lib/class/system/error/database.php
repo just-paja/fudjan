63ff6968732ed7e26a926653084e7babb652c544
just-paja <just.paja@gmail.com>
2016-03-26 21:36:44 +0100

    Database error, concat to string

--- lib/class/system/error/database.php
+++ lib/class/system/error/database.php
@@ -2,26 +2,26 @@
 
 namespace System\Error
 {
-	class Database extends \System\Error
-	{
-		function __construct()
-		{
-			$d = func_get_args();
-			$msg = strtolower($d[0]);
+  class Database extends \System\Error
+  {
+    function __construct()
+    {
+      $d = func_get_args();
+      $msg = strtolower($d[0]);
 
-			if (strpos($msg, 'duplicate') !== false || (isset($d[1]) && strpos(strtolower($d[1]), 'duplicate') !== false)) {
-				array_unshift($d, 'Cannot insert data because of duplicate unique key.');
-			} elseif (strpos($msg, 'syntax') !== false) {
-				array_unshift($d, 'Cannot run query because of syntax error.');
-			} elseif (strpos($msg, 'table') !== false && strpos($msg, 'exist')) {
-				array_unshift($d, 'Table does not exist.');
-			} elseif (strpos($msg, 'unknown column') === 0) {
-				array_unshift($d, 'Missing column');
-			} else {
-				array_unshift($d, 'Unhandled database error');
-			}
+      if (strpos($msg, 'duplicate') !== false || (isset($d[1]) && strpos(strtolower($d[1]), 'duplicate') !== false)) {
+        array_unshift($d, 'Cannot insert data because of duplicate unique key.');
+      } elseif (strpos($msg, 'syntax') !== false) {
+        array_unshift($d, 'Cannot run query because of syntax error.');
+      } elseif (strpos($msg, 'table') !== false && strpos($msg, 'exist')) {
+        array_unshift($d, 'Table does not exist.');
+      } elseif (strpos($msg, 'unknown column') === 0) {
+        array_unshift($d, 'Missing column');
+      } else {
+        array_unshift($d, 'Unhandled database error');
+      }
 
-			call_user_func_array(array('parent', '__construct'), $d);
-		}
-	}
+      parent::__construct(implode(', ', $d));
+    }
+  }
 }
